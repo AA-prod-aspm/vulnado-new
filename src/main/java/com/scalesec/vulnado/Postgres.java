04044a9b9004bea43c0351c1858aea3cd8b3993b
Menaga Muthuramalingam <107127252+Menaga12@users.noreply.github.com>
2026-04-20 18:48:29 +0530

    Update Postgres.java

--- src/main/java/com/scalesec/vulnado/Postgres.java
+++ src/main/java/com/scalesec/vulnado/Postgres.java
@@ -44,7 +44,7 @@ public class Postgres {
 
             // Insert seed data
             insertUser("admin", "!!SuperSecretAdmin!!");
-            insertUser("alice", "AlicePassword!");
+
             insertUser("bob", "BobPassword!");
             insertUser("eve", "$EVELknev^l");
             insertUser("rick", "!GetSchwifty!");
